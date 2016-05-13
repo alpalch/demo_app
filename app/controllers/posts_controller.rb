1c06c85491996e9007b183e76c0019eca4f03c4b
alpalch <alpalch@gmail.com>
2016-05-13 10:06:40 +0300

    pagination done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.reverse_order.page(params[:page]).per(25)
   end
 
   # GET /posts/1
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+(1..1000).each do |i|
+  Post.create!(title: %{'Lorem ipsum'}, content: %{'Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+               Praesent egestas lobortis blandit. Nunc at tortor nibh. Sed eget
+               commodo erat. Nam laoreet auctor lacus, sed vehicula tortor accumsan ut.
+               Nulla facilisi. Sed et leo quis quam ornare tempus. Vestibulum mi velit,
+               euismod non vehicula quis, dictum sit amet orci. Curabitur dui justo,
+                nec tempor ac, ultrices a odio. Nunc quis libero at risus suscipit suscipit a ut eros.'})
+end
